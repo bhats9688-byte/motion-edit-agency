1c80c8e8082150b40f99a062c0f0fa3114cfe75b
bhats9688-byte <bhats9688@gmail.com>
2026-03-12 15:29:18 +0530

    Add getPosts function to retrieve project data

---
+++ utils/utils.ts
@@ -0,0 +1,10 @@
+export function getPosts() {
+  return [
+    {
+      id: 1,
+      title: "Video Editing Project 1",
+      description: "Sample project",
+      image: "/images/project1.jpg",
+    }
+  ];
+}
